07510ba17b74b0e9ce70876edaaa39564c15cccf
crislav10 <cristianlav10@gmail.com>
2021-08-27 16:05:32 +0000

    se agregan Templates, url.py, app=polls,manejo http request

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question #importando base de datos de polls
 
+#Sin shorcut(template,http404)
+#from django.template import loader
+#from django.http import Http404
+
+
+#sin shorcut (template)
+# def index(request):
+#     latest_question_list=Question.objects.order_by('-pub_date')[:5]
+#     #output=', '.join([q.question_text for q in latest_question_list]) # une el string y separa con comas y espacio
+#     template=loader.get_template('polls/index.html')
+#     context={'latest_question_list':latest_question_list,
+#     }
+#     return HttpResponse(template.render(context,request))
 
 def index(request):
-    return HttpResponse("Hello, world. 022823b8 is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+#Sin shorcut (Http4004)
+# def detail (request,question_id):
+#     try:
+#         question=Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request,"polls/detail.html",{'question':question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request,question_id):
+    response="You're looking at the results of question %s."
+    return HttpResponse(response %question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s." %question_id)
+
+def owner(request):
+    return HttpResponse("Hello, world. 70b508af is the polls index.")
